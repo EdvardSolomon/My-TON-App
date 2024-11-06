2de933ca5a841e966612f17f07faa8e524ed4cb8
EdvardSolomon <sobakaikot342@gmail.com>
2024-11-06 14:57:16 +0200

    Migration to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 import FirstPage from "./pages/FirstPage";
