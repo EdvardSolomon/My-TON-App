baa249137de7147d838d7b5e992fe5972ad06a5e
EdvardSolomon <sobakaikot342@gmail.com>
2024-11-06 14:55:12 +0200

    ConnectWalletButton: latest changes

--- src/components/ConnectWalletButton/index.tsx
+++ src/components/ConnectWalletButton/index.tsx
@@ -1,45 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useTonConnectModal, TonConnectUI } from "@tonconnect/ui-react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {
+  TonConnectButton,
+  useTonConnectUI,
+  useTonWallet,
+} from "@tonconnect/ui-react";
 import { connectWallet, disconnectWallet } from "../../store/walletSlice";
-import { RootState } from "../../store/store";
 import styles from "./ConnectWalletButton.module.scss";
 
 const ConnectWalletButton: React.FC = () => {
   const dispatch = useDispatch();
-  const walletAddress = useSelector((state: RootState) => state.wallet.address);
-  const { open, close } = useTonConnectModal();
-  const [tonConnectUI] = useState(() => new TonConnectUI());
+  const wallet = useTonWallet();
+  const [tonConnectUI] = useTonConnectUI();
 
-  const handleConnect = () => {
-    open();
-  };
+  useEffect(() => {
+    if (wallet) {
+      dispatch(connectWallet(wallet.account.address));
+    } else {
+      dispatch(disconnectWallet());
+    }
+  }, [wallet, dispatch]);
 
-  const handleDisconnect = () => {
+  const handleDisconnect = async () => {
+    await tonConnectUI.disconnect();
     dispatch(disconnectWallet());
   };
 
-  useEffect(() => {
-    tonConnectUI.onStatusChange((wallet) => {
-      if (wallet) {
-        dispatch(connectWallet(wallet.account.address));
-        close();
-      } else {
-        dispatch(disconnectWallet());
-      }
-    });
-  }, [dispatch, tonConnectUI, close]);
-
   return (
-    <div>
-      <button
-        onClick={walletAddress ? handleDisconnect : handleConnect}
-        className={`${styles.button} ${
-          walletAddress ? styles.connected : styles.disconnected
-        }`}
-      >
-        {walletAddress ? "Disconnect" : "Connect Wallet"}
-      </button>
+    <div className={styles.container}>
+      {wallet ? (
+        <button
+          onClick={handleDisconnect}
+          className={styles.disconnectButton}
+        >
+          Disconnect
+        </button>
+      ) : (
+        <TonConnectButton className={styles.connectButton} />
+      )}
     </div>
   );
 };
